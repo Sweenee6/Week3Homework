3470cf5a33e1d146160c63061c6f466d3dc92c1b
Sweenee6 <73003974+Sweenee6@users.noreply.github.com>
2021-09-15 17:30:56 -0400

    Basic ball movement and out of bounds
    Ball has moves in random direction and is deleted when out of bounds.

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -4,7 +4,36 @@ public class Ball : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody2D rigidBody = null;
+    [SerializeField]
+    private float speed = 1.0f;
+
+    private Vector3 currentDirection = Vector3.zero;
+
+    private void Awake()
+    {
+        currentDirection = new Vector3(Random(), Random(), 0).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        var newDelta = currentDirection * Time.deltaTime * speed;
+        rigidBody.MovePosition(transform.position + newDelta);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        currentDirection = Vector2.Reflect(currentDirection, collision.contacts[0].normal);
+    }
+
+    private int Random()
+    {
+        return UnityEngine.Random.Range(0, 2) == 0 ? -1 : 1;
+    }
 
     //TODO: Add code to move ball along with code to delete pieces upon colliding with one
     //Ball should only move once its been launched
+
+
+
+
 }
--- Assets/Scripts/Bounds/OutOfBounds.cs
+++ Assets/Scripts/Bounds/OutOfBounds.cs
@@ -6,5 +6,13 @@ public class OutOfBounds : MonoBehaviour
     {
         //TODO: Implement functionality to reset the game somehow.
         // Resetting the game includes destroying the out of bounds ball and creating a new one ready to be launched from the paddle
+
+        // Look to see if collision is with the ball
+        if (collision.CompareTag("Ball"))
+        {
+            // destroy the ball
+            Destroy(collision.gameObject);
+        }
+
     }
 }
